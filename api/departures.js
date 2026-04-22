22322ddde1a30e27e6111e1b2cf845c40a4f28eb
adamcaban89 <adam.caban@gmail.com>
2026-04-22 09:43:54 +0200

    Add files via upload

---
+++ api/departures.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const { stopId, stopNr, line } = req.query;
+
+  const url = `https://api.um.warszawa.pl/api/action/dbtimetable_get/?id=e923fa0e-d96c-43f9-ae6e-60518c9f3238&busstopId=${stopId}&busstopNr=${stopNr}&line=${line}&apikey=${process.env.API_KEY}`;
+
+  try {
+    const r = await fetch(url);
+    const data = await r.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: "proxy error" });
+  }
+}
